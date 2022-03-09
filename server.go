1165a4a760437f9b4d7c82a8188890cae72adf9a
Ylias ASSANY <yassany@bocasay.com>
2022-03-09 16:52:04 +0300

    add CLI

--- server.go
+++ server.go
@@ -9,11 +9,12 @@ import (
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/urfave/cli/v2"
 )
 
-const defaultPort = "8080"
+var defaultPort = "8080"
 
-func main() {
+func launchServer() {
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
@@ -26,4 +27,25 @@ func main() {
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "Custom GraphQL Server"
+	app.Usage = "A little graphql server for meteo"
+	app.Flags = []cli.Flag{
+		&cli.StringFlag{
+			Name:    "port",
+			Aliases: []string{"p"},
+			Value:   "8080",
+			Usage:   "port in wich the server will listen",
+		},
+	}
+	app.Action = func(c *cli.Context) error {
+		defaultPort = c.String("port")
+		launchServer()
+		return nil
+	}
+
+	app.Run(os.Args)
 }
